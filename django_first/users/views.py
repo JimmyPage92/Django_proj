27838873e99a30d71ffc726fd4d0b373f6a5f228
JimmyPage92 <mipas10@gmail.com>
2022-09-22 19:08:38 +0200

    commit My Django

--- django_first/users/views.py
+++ django_first/users/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render,redirect
-from .forms import UserRegistration
+from .forms import UserRegistration, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
-
-
-
 
 def register(request):
     if request.method == 'POST':
@@ -19,4 +16,18 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == "POST":
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+
+    return render(request, 'users/profile.html', {'user_form': user_form, 'profile_form': profile_form})
+
