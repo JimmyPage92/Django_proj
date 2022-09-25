eb5fc158b5cdb45148f55a87d9bf383aa0918789
JimmyPage92 <mipas10@gmail.com>
2022-09-25 20:36:07 +0200

    commit My Django

--- django_first/users/models.py
+++ django_first/users/models.py
@@ -15,7 +15,7 @@ class Profile(models.Model):
         img = Image.open(self.image.path)
 
         if img.height > 300 or img.width > 300:
-            output_size = (200,200)
+            output_size = (150,150)
             img.thumbnail(output_size)
             img.save(self.image.path)
 
