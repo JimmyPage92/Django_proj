73397932297ce66bba6da30c3b6ef79290880849
JimmyPage92 <mipas10@gmail.com>
2022-09-27 20:25:15 +0200

    commit My Django

--- django_first/blog/urls.py
+++ django_first/blog/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from . import views as views
+from .views import ArticleListView, ArticleDetailView, ArticleCreateView
+from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
-    path('about/', views.about, name='blog-about')
+    path('', ArticleListView.as_view(), name='blog-home'),
+    path('about/', views.about, name='blog-about'),
+    path('article/<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
+    path('article/new/', ArticleCreateView.as_view(), name='article-create'),
 ]
--- django_first/blog/views.py
+++ django_first/blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Article
+from django.views.generic.list import ListView # tu bedziemy przechowywac wszystkie artykuly z bloga
+from django.views.generic.detail import DetailView
+from django.views.generic import CreateView
 
 # Create your views here.
 
@@ -8,3 +11,23 @@ def home(request):
 
 def about(request):
     return render(request,'blog/about.html', {'title': 'About'})
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-data_posted']
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'blog/article.html'
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = 'blog/article_form.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
