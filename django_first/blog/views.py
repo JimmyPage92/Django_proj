811c1aae8ccf42a6a9bb1c5343d843c706658b98
JimmyPage92 <mipas10@gmail.com>
2022-09-28 19:36:47 +0200

    commit My Django

--- django_first/blog/views.py
+++ django_first/blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import Article
-from django.views.generic.list import ListView # tu bedziemy przechowywac wszystkie artykuly z bloga
+from django.views.generic.list import ListView #wszystkie artykuly z bloga lista wyświetlanych artykułów na blogu
 from django.views.generic.detail import DetailView
 from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -18,16 +19,15 @@ class ArticleListView(ListView):
     context_object_name = 'posts'
     ordering = ['-data_posted']
 
-
 class ArticleDetailView(DetailView):
     model = Article
     template_name = 'blog/article.html'
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin,CreateView): # przypisywać utworzony artykuł doaktualnie zalogowanego użytkownika: form.instance.author =self.request.user
     model = Article
     template_name = 'blog/article_form.html'
     fields = ['title', 'content']
 
-    def form_valid(self, form):
+    def form_valid(self, form):#metoda ktora automatycznie przypisuje nowy artykul do autora-uzytkownika serwisu
         form.instance.author = self.request.user
         return super().form_valid(form)
